9de07431b9a6be12bb4f0cb79a963191959cc3f7
prateek <prateek.tejasvi@gmail.com>
2025-10-18 03:57:06 +0530

    Made a fix:for issue base64

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,21 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import urllib
 import base64
+import torch
 
+if not hasattr(torch.Tensor, "_old_H"):
+    torch.Tensor._old_H = torch.Tensor.H
+
+    @property
+    def safe_H(self):
+        if self.ndim < 2:
+            return self
+        return self._old_H
+
+    torch.Tensor.H = safe_H
 
 app = Flask(__name__)
 cors = CORS(app)
-
 
 learn_inf = load_learner("bears.pkl")
 
@@ -18,16 +28,15 @@ def search_by_base64_image(b64imgstring):
     """
     destination_filename = "temp.jpg"
     with open(destination_filename, "wb") as image:
-        image.write(base64.decodebytes(b64imgsting.encode()))
-    val = learn_inf.predict("temp.jpg")
+        image.write(base64.decodebytes(b64imgstring.encode()))
+    val = learn_inf.predict(destination_filename)
     return [val[0], val[2][0].item(), val[2][1].item(), val[2][2].item()]
 
 
 def search_by_url(url):
     dest = "temp.jpg"
-    # download_url(url, dest)
     urllib.request.urlretrieve(url, dest)
-    val = learn_inf.predict("temp.jpg")
+    val = learn_inf.predict(dest)
     return [val[0], val[2][0].item(), val[2][1].item(), val[2][2].item()]
 
 
@@ -49,7 +58,7 @@ def bear_hunt():
 @app.route("/api/url/bear_base64", methods=["POST"])
 @cross_origin()
 def bear_hunt_base64():
-    data = request.json
-    ans = search_by_base64_image(data["img_base64_string"])
+    b64data = request.get_data(as_text=True)
+    ans = search_by_base64_image(b64data)
     print(ans)
     return {"prediction": ans}
